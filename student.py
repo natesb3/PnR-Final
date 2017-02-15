f656d3e5149a6fb20729968adffbe6d66f1818c8
sudberryn <sudberryn@gilmour.org>
2017-02-15 10:55:58 -0500

    pigo servo, student dance1

--- student.py
+++ student.py
@@ -71,10 +71,10 @@ class GoPiggy(pigo.Pigo):
     def shimmy(self):
         print('shimmy')
         for x in range(3):
-        self.servo(30)
-        self.encR(3)
-        self.servo(140)
-        self.encL(3)
+            self.servo(30)
+            self.encR(3)
+            self.servo(140)
+            self.encL(3)
 
     ########################
     ### MAIN LOGIC LOOP - the core algorithm of my navigation
