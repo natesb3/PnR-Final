43c9bc21d89645d28fadb92fbdc48b61ea2d2450
sudberryn <sudberryn@gilmour.org>
2017-04-12 10:42:58 -0400

    sweep scan

--- student.py
+++ student.py
@@ -18,7 +18,7 @@ class GoPiggy(pigo.Pigo):
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 90
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
-        self.STOP_DIST = 30
+        self.STOP_DIST = 60
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.LEFT_SPEED = 120
         # YOU DECIDE: What left motor power helps straighten your fwd()?
@@ -170,7 +170,7 @@ class GoPiggy(pigo.Pigo):
         while True:
             if self.is_clear():
                 self.cruise()
-            answer = self.encFwd(5)
+            answer = self.encF(5)
             if answer == "left":
                 self.encL(6)
             elif answer == "right":
