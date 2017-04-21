af2e575e4dfa8855886ff077aff44331ab239676
sudberryn <sudberryn@gilmour.org>
2017-04-21 10:26:23 -0400

    Stop Distance

--- student.py
+++ student.py
@@ -18,7 +18,7 @@ class GoPiggy(pigo.Pigo):
         # Our servo turns the sensor. What angle of the servo( ) method sets it straight?
         self.MIDPOINT = 90
         # YOU DECIDE: How close can an object get (cm) before we have to stop?
-        self.STOP_DIST = 40
+        self.STOP_DIST = 30
         # YOU DECIDE: What left motor power helps straighten your fwd()?
         self.LEFT_SPEED = 110
         # YOU DECIDE: What left motor power helps straighten your fwd()?
@@ -115,7 +115,7 @@ class GoPiggy(pigo.Pigo):
         elif self.turn_track < 0:
             print('I must have turned left a lot no I should turn tp self.encR(??)')
             abs(self.turn_track)
-            self.encR(turn)
+            self.encR()
 
 
 
